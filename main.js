6c8c75caef9a2e07ecd6475941b7fec94828a956
Salvador Jesús Megías Andreu <salvadorjmegias@gmail.com>
2022-02-07 17:10:46 +0100

    cambios introducidos guapos y funcionales: particles, cambio de imagenes de fondo, colores de fondo , links a zonas de la website

--- main.js
+++ main.js
@@ -195,7 +195,7 @@ document.addEventListener("click", (e) => {
 
 // typed.js
 var options = {
-    strings: ["Useful", "Powerful", "Talented"],
+    strings: ["Enthusiastic", "Ambitious", "Open Minded"],
     typeSpeed: 80,
     fadeOut: true,
     loop: true,
@@ -203,3 +203,114 @@ var options = {
 };
 
 var typed = new Typed('.element', options);
+
+
+
+
+
+
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 75,
+        "density": {
+          "enable": true,
+          "value_area": 550
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 4,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 20,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 170,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 1200,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
